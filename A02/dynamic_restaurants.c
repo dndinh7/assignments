07d2e7f9b374d429ee8f2c1264dfa1d1b0561e83
David Dinh <dndinh7@gmail.com>
2022-09-17 19:18:19 -0400

    finished the prototypes of dynamic_restaurants and slow

--- A02/dynamic_restaurants.c
+++ A02/dynamic_restaurants.c
@@ -1,5 +1,51 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct restaurant {
+  char name[32];
+  int open_time;
+  int close_time;
+  float stars;
+  int id;
+};
+
+
+void print_restaurants(struct restaurant* restaurant) {
+  printf("%d) %-29s Opens: %2d:00    Closes: %2d:00    Rating: %.2f\n",
+    restaurant->id, restaurant->name, restaurant->open_time,
+    restaurant->close_time, restaurant->stars);
+
+}
+
 
 int main() {
+  int num_restaurants;
+
+
+  printf("Enter number of restaurants: ");
+  scanf("%d%*c", &num_restaurants);
+
+  struct restaurant* restaurants= malloc(sizeof(struct restaurant) * num_restaurants);
+  
+  for (int i= 0; i < num_restaurants; i++) {
+    restaurants[i].id= i;
+    printf("Enter a name: ");
+    scanf("%[^\n]%*c", restaurants[i].name);
+    printf("Open time (24 hours): ");
+    scanf("%d%*c", &restaurants[i].open_time);
+    printf("Closing time (24 hours): ");
+    scanf("%d%*c", &restaurants[i].close_time);
+    printf("Stars (0 to 5): ");
+    scanf("%e%*c", &restaurants[i].stars);
+
+  }
+
+  for (int i= 0; i < num_restaurants; i++) {
+    print_restaurants(&restaurants[i]);
+  }
+
+
+  free(restaurants);
   return 0;
 }
--- A02/slow.c
+++ A02/slow.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
 
 int main() {
+  char buff[32];
+  int pause;
+  int size_slow;
+
+  printf("Pause length: ");
+  scanf("%d", &pause);
+
+  printf("Text: ");
+  scanf("%s", buff);
+
+  // you add pause number of periods onto buff and add 1 for terminating char
+  size_slow= (pause+1)*strlen(buff)+1;
+  char* slow= malloc(size_slow);
+
+  for (int i= 0; i < size_slow-1; i++) {
+    if (i % (pause+1) == 0) {
+      slow[i]= buff[i/(pause+1)];
+    } else {
+      slow[i]= '.';
+    }
+  }
+
+  slow[size_slow-1]= '\0';
+
+  printf("%s\n", slow);
+
+  free(slow);
+  slow= NULL;
+
   return 0;
 }
