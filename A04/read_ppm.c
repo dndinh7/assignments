53696a74be71fa6089f9fce3ba28f3eaadf0e4da
David Dinh <dndinh7@gmail.com>
2022-10-01 22:52:44 -0400

    I have a draft of the ppm, but I am not happy, since it feels like im hard coding the header

--- A04/read_ppm.c
+++ A04/read_ppm.c
@@ -6,7 +6,107 @@
 // Choose *one* to implement
 
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
-  return NULL;
+  FILE* input= fopen(filename, "r");
+  if (!input) {
+    printf("Input file %s could not be opened", filename);
+    return NULL;
+  }
+
+  // delimiter
+  char *whitespace= " \t\f\r\v\n";
+
+  
+  struct ppm_pixel* image;
+  //char magic_num[16];
+  int max_color= 0;
+  int row= 0;
+
+  // no line should have more than 70 chars
+  char line[80];
+  char *token;
+  
+
+  // hard code to get information about ia
+
+  while (fgets(line, 80, input) != NULL && row < 3) {
+    token= strtok(line, whitespace);
+    if (token[0] == '#') {
+      continue;
+    }
+    if (row == 0) { // check magic number
+      if (token[0] != 'P' && (token[1] != '3' || token[1] != '6')) {
+        printf("Did not get a valid magic number.\n");
+        return NULL;
+      }
+      row++;
+      token= strtok(NULL, whitespace);
+      if (token == NULL) {
+        continue;
+      }
+
+    }
+    if (row == 1) { // check grid sizes
+      *w= atoi(token);
+      if (*w == 0) {
+        printf("Not a valid width.\n");
+        return NULL;
+      }
+
+      token= strtok(NULL, whitespace);
+      if (token == NULL) {
+        fgets(line, 80, input);
+        token= strtok(line, whitespace);
+      }
+      
+      *h= atoi(token);
+      if (*h == 0) {
+        printf("Not a valid height.\n");
+        return NULL;
+      }
+
+      row++;
+      token= strtok(NULL, whitespace);
+      if (token == NULL) {
+        continue;
+      }
+    }
+    // check max color value
+    max_color= atoi(token);
+    if (max_color == 0) {
+      printf("Not a valid max color value.\n");
+      return NULL;
+    }
+    row++;
+    token= strtok(NULL, whitespace);
+    break;
+  }
+  
+  // if it passes this header, then we know there is only one white space after
+  image= malloc(sizeof(struct ppm_pixel) * *w * *h);
+  if (image == NULL) {
+    printf("Not enough space to allocate the image.\n");
+    return NULL;
+  }
+  
+  unsigned char red;
+  unsigned char green;
+  unsigned char blue;
+  for (int i= 0; i < *w; i++) {
+    for (int j= 0; j < *h; j++) {
+      red= fgetc(input);
+      green= fgetc(input);
+      blue= fgetc(input);
+      image[i* *h + j].red= red;
+      image[i* *h + j].green= green;
+      image[i* *h + j].blue= blue;
+    }
+  }
+
+
+
+  fclose(input);
+
+  return image;
 }
 
 struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
--- A04/test_ppm.c
+++ A04/test_ppm.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "read_ppm.h"
+#include <string.h>
 
 int main() {
   // load feep-raw and print out the grid of pixels
+  int w= 0;
+  int h= 0;
+  char file[64];
+  struct ppm_pixel* image;
+
+  strcpy(file, "feep-raw.ppm");
+  image= read_ppm("feep-raw.ppm", &w, &h);
+  
+  printf("Testing file feep-raw.ppm: width %d and height %d\n", w, h);
+
+  for (int i= 0; i < w; i++) {
+    for (int j= 0; j < h; j++) {
+      printf("(%u, %u, %u) ", image[i*h + j].red, image[i*h +j].green,
+          image[i*h + j].blue);
+
+    }
+    printf("\n");
+  }
+
+  free(image);
   return 0;
 }
-
