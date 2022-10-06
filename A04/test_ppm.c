b5ce0717542bbb69cabcaee160366d3a6ca66a40
David Dinh <dndinh7@gmail.com>
2022-10-06 18:36:20 -0400

    re-added test_ppm.c

---
+++ A04/test_ppm.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "read_ppm.h"
+#include <string.h>
+
+int main() {
+    // load feep-raw and print out the grid of pixels
+    int w= 0;
+    int h= 0;
+    char file[64];
+    struct ppm_pixel* image;
+
+    strcpy(file, "feep-raw.ppm");
+    image= read_ppm(file, &w, &h);
+      
+    printf("Testing file feep-raw.ppm: width %d and height %d\n", w, h);
+
+    for (int i= 0; i < h; i++) {
+      for (int j= 0; j < w; j++) {
+        printf("(%u, %u, %u) ", image[i*w + j].red, image[i*w +j].green,
+                                image[i*w + j].blue);
+
+      }
+    printf("\n");
+    }
+
+    free(image);
+    return 0;
+}
