41a84fe82a773213a90d42913041f6ec9aaca672
David Dinh <dndinh7@gmail.com>
2022-10-14 18:21:18 -0400

    finished write ppm, but perhaps I skirted some technical stuff?

--- A05/write_ppm.c
+++ A05/write_ppm.c
@@ -5,7 +5,15 @@
 // implement one
 
 void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
+  FILE* output;
+  output= fopen(filename, "w");
+  
+  fprintf(output, "P6\n%d %d\n255\n", w, h);
+  
+  fwrite(pixels, 1, sizeof(struct ppm_pixel)*w*h, output);
+  
 
+  fclose(output);
 }
 
 void write_ppm_2d(const char* filename, struct ppm_pixel** pixels, int w, int h) {
