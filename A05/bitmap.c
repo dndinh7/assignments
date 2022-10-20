a4392293428ffd25af991fa16f19a8a1c5b88906
David Dinh <dndinh7@gmail.com>
2022-10-20 17:13:47 -0400

    changed the bitmap to @ sign

--- A05/bitmap.c
+++ A05/bitmap.c
@@ -5,16 +5,13 @@ int main() {
   scanf(" %lx", &img);
   printf("Image (unsigned long): %lx\n", img);
 
-  
-
-  // todo: your code here
   unsigned long mask= 0x1ul;
   unsigned long shift_mask;
   for (int i= 63; i >= 0; i--) {
     shift_mask= mask << i;
 
     if (shift_mask & img) { // if bit is a 1 (black pixel)
-      printf("# ");
+      printf("@ ");
     } else { // if bit is a 0 (white pixel)
       printf("  "); 
     }
@@ -22,5 +19,6 @@ int main() {
     if (i % 8 == 0) printf("\n");
 
   }
+  printf("-----\n");
   return 0;
 }
