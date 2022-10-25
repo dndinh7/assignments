d4e9daeb94a637ab021a665918958e44347a3e36
David Dinh <dndinh7@gmail.com>
2022-10-25 15:05:32 -0400

    finished scramble

--- Jam03/scramble.c
+++ Jam03/scramble.c
@@ -1,11 +1,42 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-  char message[32];
+  char message[64];
   printf("Please enter a phrase: ");
-  fgets(message, 31, stdin);
-  message[31] = '\0';
+  scanf("%[^\n]%*c", message);
 
-  // todo: modify message using bitwise operations
+  unsigned char mask3= 0x1 << 2;
+  unsigned char mask4= 0x1 << 3;
+
+  for (int i= 0; i < strlen(message); i++) {
+    unsigned char bit3= message[i] & mask3;
+    unsigned char bit4= message[i] & mask4;
+    if (bit3) {
+      bit3 <<= 1;
+      message[i]|= bit3;
+    } else {
+      bit3= 0x1 << 3;
+      bit3= ~bit3;
+      message[i]&= bit3;
+    }
+
+    if (bit4) {
+      bit4 >>= 1;
+      message[i]|= bit4;
+    } else {
+      bit4= 0x1 << 2;
+      bit4= ~bit4;
+      message[i]&= bit4;
+    }
+  }
+
   printf("scramble: %s\n", message);
+
+
+  return 0;
+
+
+
 }
